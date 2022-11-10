e81096503baec2bcfe05e2e7319948b65baf911e
turcumanhoria <turcumanhoria@gmail.com>
2022-11-10 11:21:22 +0200

    refactor(data): change json to pandas

--- scripts/preprocess-data/cath.py
+++ scripts/preprocess-data/cath.py
@@ -1,4 +1,4 @@
-import json
+import pandas as pd
 
 annotations = {}
 
@@ -8,33 +8,48 @@ with open('res/cath/raw/cath-domain-list.txt', 'r') as f:
             words = line.split()
             annotations[words[0]] = [int(words[1]), int(words[2]), int(words[3]), int(words[4])]
 
-for src, dst in [('cath_v430_trainS95_nrTopoBetween.fa', 'train/'),
-                 ('cath_v430_finalVal_nrTopoBetween_nrHomoWithin.fa', 'val/'),
-                 ('cath_v430_finalTest_nrTopoBetween_nrHomoWithin.fa', 'test/')]:
+# TRAIN
 
-    seqs_with_anotation = []
-    seqs_without_anotation = []
+# all sequences in this file are annotated
+with open('res/cath/raw/cath_v430_trainS95_nrTopoBetween.fa', 'r') as f:
+    annotated_seqs = {
+        "id": [],
+        "annotation": [],
+        "seq": []
+    }
+
+    lines = f.readlines()
+
+    for i in range(0, len(lines), 2):
+        id = lines[i][1:-1]
+
+        annotated_seqs['id'].append(id)
+        annotated_seqs['annotation'].append(annotations[id])
+        annotated_seqs['seq'].append(lines[i + 1][:-1])
+
+    df = pd.DataFrame(annotated_seqs) 
+    df.to_csv('res/cath/train/annotated_seqs.csv')
+
+
+# VALIDATION, TEST
+
+# all sequences in these files are annotated            
+for src, dst in [('cath_v430_finalVal_nrTopoBetween_nrHomoWithin.fa', 'val.csv'),
+                 ('cath_v430_finalTest_nrTopoBetween_nrHomoWithin.fa', 'test.csv')]:
     
     with open('res/cath/raw/' + src, 'r') as f:
         lines = f.readlines()
 
+        annotated_seqs = {
+            "id": [],
+            "annotation": []
+        }
+
         for i in range(0, len(lines), 2):
             id = lines[i][1:-1]
+
+            annotated_seqs['id'].append(id)
+            annotated_seqs['annotation'].append(annotations[id])
             
-            if id in annotations:
-                seqs_with_anotation.append({
-                    'id' : id,
-                    'annotation' : annotations[id],
-                    'seq' : lines[i + 1][:-1]
-                })
-            else:
-                seqs_without_anotation.append({
-                    'id' : id,
-                    'seq' : lines[i + 1][:-1]
-                })
-
-        with open('res/cath/' + dst + 'seqs_with_annotation.json', 'w') as f2:
-            json.dump(seqs_with_anotation, f2)
-
-        with open('res/cath/' + dst + 'seqs_without_annotation.json', 'w') as f2:
-            json.dump(seqs_without_anotation, f2)
+        df = pd.DataFrame(annotated_seqs) 
+        df.to_csv('res/cath/' + dst)
